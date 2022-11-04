6f3cd735376719fcdab9c3f9ab2714f52669f82b
JTF <JTFoster13@yahoo.com>
2022-11-04 12:54:34 -0500

    Added some comments about the F1 score and imported some useful packages

---
+++ main.py
@@ -0,0 +1,13 @@
+from sklearn.metrics import f1_score
+import numpy as np
+
+#f1 = (2 * P-micro * Rmicro) / P-micro + R-micro
+
+#also referred as f1 = (2 * (Precision * Recall)) / (Precision + Recall)
+
+#Precision = Correct positive predictions relative to total positive predictions
+#P = TP / (TP + FP)
+
+#Recalls = Correct positive predictions relative to total actual positives
+#Recalls = TP / (TP + FN)
+
